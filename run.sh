b4b0587d8822ac3daa7fdcd5f7f5905484fc2206
Özcan Yarımdunya <ozcanyd@gmail.com>
2019-03-26 00:54:46 +0300

    Change structure

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4 --reload
